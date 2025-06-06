ee01fadc55708ca93983fc483694d7941add29b8
Leccooo <torrat90@gmail.com>
2025-06-06 18:32:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.all('/player/*', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Rabbid Loginurl Created @Lecco');
 });
 
 app.listen(5000, function () {
